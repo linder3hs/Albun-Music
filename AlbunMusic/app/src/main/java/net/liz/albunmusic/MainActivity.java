c9861b7cfe332525e0a18aca2e4be574f0aa899e
Linder Hassinger <linderhassinger00@gmail.com>
2017-08-21 23:44:39 -0500

    Albun

---
+++ AlbunMusic/app/src/main/java/net/liz/albunmusic/MainActivity.java
@@ -0,0 +1,56 @@
+package net.liz.albunmusic;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    //Declaro una variable
+    ImageView dontCry, clud, patience, estranged;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Instanciando
+        dontCry = (ImageView) findViewById(R.id.dont_cry);
+        clud    = (ImageView) findViewById(R.id.cloud);
+        patience = (ImageView) findViewById(R.id.patience);
+        estranged = (ImageView) findViewById(R.id.estranged);
+
+        dontCry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        clud.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        patience.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        estranged.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
